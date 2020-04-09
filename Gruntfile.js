c2c863985c40659e4ad342d2e3aba0623f0a554c
gaoruixue1215 <1171331232@qq.com>
2020-04-09 20:14:24 +0800

    V0.5

--- Gruntfile.js
+++ Gruntfile.js
@@ -81,9 +81,9 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    //grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-copy');
-   // grunt.loadNpmTasks('grunt-usemin');
+    grunt.loadNpmTasks('grunt-usemin');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
     grunt.registerTask('unitTest', ['mocha']);
